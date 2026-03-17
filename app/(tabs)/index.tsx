294489fd16879a26d930b269436767563626857c
tythomassen <tyler.thomassen@icloud.com>
2026-03-17 19:13:19 -0400

    Update app with new assets and configurations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,14 @@
-import { Image } from 'expo-image';
-import { View, Text, StatusBar } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, StatusBar } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-red-500">
+        Genius App!
+      <StatusBar style="auto" />
       </Text>
-    </View>
+    </SafeAreaView>
   );
 }
+
--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
+const { defineConfig, globalIgnores } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
 module.exports = defineConfig([
+  globalIgnores(['dist/*']),
   expoConfig,
   {
-    ignores: ['dist/*'],
+    files: ['babel.config.js'],
+    languageOptions: {
+      globals: globals.node,
+    },
   },
 ]);
