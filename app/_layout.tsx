ed2e1d694df3d6f60ebe44078da88892d40497b4
tythomassen <tyler.thomassen@icloud.com>
2026-03-17 23:25:17 -0400

    Hide the (auth) header in the root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -41,6 +41,9 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(root)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
